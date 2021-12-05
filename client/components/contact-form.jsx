738c53be90be3000394356067c6e52366549f87d
Kenneth Wang <yunfeiw@me.com>
2021-12-05 09:33:48 +0000

    back-end finished

--- client/components/contact-form.jsx
+++ client/components/contact-form.jsx
@@ -8,13 +8,16 @@ export default class ContactForm extends React.Component {
       sender: '',
       email: '',
       msgTitle: '',
-      content: ''
+      content: '',
+      msgStatus: false
     };
     this.openForm = this.openForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   openForm() {
-    this.setState({ formOpen: !this.state.formOpen });
+    this.setState({ formOpen: !this.state.formOpen, msgStatus: false });
   }
 
   handleChange(event) {
@@ -22,22 +25,55 @@ export default class ContactForm extends React.Component {
     this.setState({ [name]: value });
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch('/api/messages/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data) {
+          this.setState({ msgStatus: true });
+        }
+      })
+      .catch(error => {
+        console.error('error', error);
+      });
+
+    this.setState({
+      sender: '',
+      email: '',
+      msgTitle: '',
+      content: ''
+    });
+
+  }
+
   render() {
     return (
       <div ref={this.props.contactMe} style={{ marginTop: '3rem' }}className="container text-center">
         <h1 className="contact-title ">Get in Touch, <span>Anytime!</span></h1>
-        <button onClick={this.openForm} className="contact-button">Contact Me</button>
-        <form className={this.state.formOpen ? 'col-three-fifth form-holder form-open' : 'col-three-fifth form-holder'} >
+        <button onClick={this.openForm} style={{ marginBottom: '0' }} className="contact-button">Contact Me</button>
+        <div className="contact-button-exp">Open | Close Contact Form</div>
+        <form onSubmit={this.handleSubmit} className={this.state.formOpen ? 'col-three-fifth form-holder form-open' : 'col-three-fifth form-holder'} >
          <div className={this.state.formOpen ? 'form-content col-four-fifth' : 'form-content col-four-fifth hidden'}>
-           <label className="form-label" htmlFor="name">Name</label>
-           <input className="form-input" id="name" name="name" type="text" value={this.state.name} placeholder="Please enter your name.."></input>
+           <label className="form-label" htmlFor="sender">Name</label>
+           <input required onChange={this.handleChange} className="form-input" id="sender" name="sender" type="text" value={this.state.sender} placeholder="Please enter your name.."></input>
            <label className="form-label" htmlFor="email">Email</label>
-           <input className="form-input" id="email" name="email" type="email" value={this.state.email} placeholder="Please enter your email.."></input>
+           <input required onChange={this.handleChange} className="form-input" id="email" name="email" type="email" value={this.state.email} placeholder="Please enter your email.."></input>
            <label className="form-label" htmlFor="msgTitle">Message Title</label>
-           <input className="form-input" id="msgTitle" name="msgTitle" type="text" value={this.state.msgTitle} placeholder="Please enter your message title.."></input>
+           <input required onChange={this.handleChange} className="form-input" id="msgTitle" name="msgTitle" type="text" value={this.state.msgTitle} placeholder="Please enter your message title.."></input>
            <label className="form-label" htmlFor="content">Message</label>
-           <textarea className="form-textarea" id="content" name="content" type="text" value={this.state.content} placeholder="Please enter your message.." />
-            <button style={{ borderBottom: '2px solid #495057' }} className="contact-button">Submit</button>
+           <textarea required onChange={this.handleChange} className="form-textarea" id="content" name="content" type="text" value={this.state.content} placeholder="Please enter your message.." />
+           <button style={{ borderBottom: '2px solid #495057' }} className="contact-button">Submit</button>
+           <div className={this.state.msgStatus ? 'msg-status' : 'msg-status hidden'}>
+              <p>Thank you for getting in touch! </p>
+              <p>I appreciate you contacting me. I will get back in touch with you soon! Have a great day!</p>
+           </div>
          </div>
         </form>
         <div className="col-three-fifth form-content-image">
--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer(props) {
   return (
     <div className="footer">
-      <div className="footer-content" >&copy; Kenneth Wang | Orange County, CA | United States</div>
+      <div className="footer-content" >&copy; Kenneth Wang | Orange County , CA | United States</div>
       <a className="footer-content" target="_blank" rel="noopener noreferrer" href='mailto:yunfeiw@me.com'>YunfeiW@me.com</a>
     </div>
   );
