f84454a82ed9e5c59a6564898752df04395e22dc
Kenneth Wang <yunfeiw@me.com>
2021-12-09 06:55:58 +0000

    post in progress

--- client/pages/blog.jsx
+++ client/pages/blog.jsx
@@ -11,9 +11,21 @@ export default class Blog extends React.Component {
 
   render() {
     return (
-      <>
-      <h1>hello</h1>
-      </>
+
+      <div className="container">
+        <div style={{ minHeight: 'calc(100vh - 190px' }}>
+          <div style={{ border: 'none', marginBottom: '1rem' }} className="title-row col-full">
+            <div className="title-content">
+              <h1><span>Welcome</span> to My Blog </h1>
+              <div className="title-sub">Please <a>Sign-in</a> to create a post</div>
+            </div>
+          </div>
+          <div className="post-creation-holder col-full">
+           <div className="post-button">Create a Post</div>
+          </div>
+        </div>
+      </div>
+
     );
   }
 }
