e244a397cdf68c66a9590307160785d55b983cb3
Kenneth Wang <yunfeiw@me.com>
2021-12-10 19:46:41 +0000

    login backend start

--- client/pages/blog.jsx
+++ client/pages/blog.jsx
@@ -6,6 +6,7 @@ export default class Blog extends React.Component {
     super(props);
     this.state = {
       sign: false,
+      errorSignIn: false,
       username: '',
       password: '',
       email: ''
@@ -13,6 +14,35 @@ export default class Blog extends React.Component {
     };
     this.signIn = this.signIn.bind(this);
     this.signUp = this.signUp.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { sign } = this.state;
+    const req = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    };
+    fetch(`/api/auth/${sign}`, req)
+      .then(res => res.json())
+      .then(result => {
+        if (sign === 'signUp') {
+          this.setState({ sign: 'signIn' });
+        } else if (result.user && result.token) {
+          this.setState({ errorSignin: false });
+        // this.props.onSignIn(result);
+        } else {
+          this.setState({ errorSignIn: true });
+        }
+      });
   }
 
   signIn() {
@@ -33,7 +63,7 @@ export default class Blog extends React.Component {
       <div className="container">
         <div className={this.state.sign === 'signIn' || this.state.sign === 'signUp' ? 'signin-modal-holder' : ' signin-modal-holder hidden'}>
           <div className="col-three-fifth signin-block">
-           <h3>{welcomeMessage}</h3>
+            <h3>{welcomeMessage}</h3>
             <form >
               <div className="input-holder">
                 <label htmlFor="username" className="sign-in-label">
@@ -48,6 +78,7 @@ export default class Blog extends React.Component {
                   // onChange={handleChange}
                   className="sign-in-input"
                   value={this.state.username}
+                  placeholder="Please enter your username..."
                 />
               </div>
               <div className="input-holder">
@@ -61,7 +92,10 @@ export default class Blog extends React.Component {
                   name="password"
                   // onChange={handleChange}
                   className="sign-in-input"
-                  value={this.state.password} />
+                  value={this.state.password}
+                  placeholder="Please enter your password..."
+                   />
+
               </div>
               <div className={this.state.sign === 'signUp' ? 'input-holder' : 'input-holder hidden'}>
                 <label htmlFor="email" className="sign-in-label">
@@ -73,7 +107,12 @@ export default class Blog extends React.Component {
                   name="email"
                   // onChange={handleChange}
                   className="sign-in-input"
-                  value={this.state.email} />
+                  value={this.state.email}
+                  placeholder="Please enter your email..."
+                   />
+              </div>
+              <div className="error-message-holder">
+               <p className={this.state.errorSignIn ? 'error-message' : 'error-message hidden'}>Incorrect username or password, please try again</p>
               </div>
               <button type="button" onClick={this.signUp} className={this.state.sign === 'signIn' ? 'sign-in-button' : 'sign-in-button hidden'}>Register now</button>
               <button type="button" onClick={this.signIn} className={this.state.sign === 'signUp' ? 'sign-in-button' : 'sign-in-button hidden'}>Sign in instead</button>
