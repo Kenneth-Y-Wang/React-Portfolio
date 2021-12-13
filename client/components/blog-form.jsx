ea1375ea06973914b163513748c9f42faf723e6a
Kenneth Wang <yunfeiw@me.com>
2021-12-13 20:35:12 +0000

    form frond end completed

--- client/components/blog-form.jsx
+++ client/components/blog-form.jsx
@@ -1,14 +1,60 @@
 import React from 'react';
+import { format } from 'date-fns';
 
 export default class BlogForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       title: '',
-      content: ''
+      content: '',
+      allPosts: []
     };
 
     this.fileInputRef = React.createRef();
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const token = window.localStorage.getItem('react-context-jwt');
+    const newTime = format(new Date(), 'yyyy-MM-dd HH:mm');
+    const form = new FormData();
+    form.append('image', this.fileInputRef.current.files[0]);
+    form.append('time', newTime);
+    form.append('title', this.state.title);
+    form.append('content', this.state.content);
+
+    fetch('/api/posts/create', {
+      method: 'POST',
+      headers: {
+        'react-context-jwt': token
+      },
+      body: form
+    })
+      .then(response => response.json())
+      .then(data => {
+        const newPost = {
+          postId: data.postId,
+          userId: data.userId,
+          title: data.title,
+          createdAt: data.createdAt,
+          content: data.content,
+          username: this.context.user.username
+        };
+        this.setState({ allPosts: this.state.allPosts.concat(newPost) });
+      })
+      .catch(error => {
+        console.error('error', error);
+      });
+
+    this.setState({
+      title: '',
+      content: ''
+    });
   }
 
   render() {
@@ -17,7 +63,7 @@ export default class BlogForm extends React.Component {
         <label htmlFor="title">Post Title</label>
         <input className="blog-form-input" required value={this.state.title} id="title" name="title" type="text" placeholder="Please enter your post title..."></input>
         <textarea required value={this.state.content} id="content" name="content" placeholder="Please enter your post..."></textarea>
-        <label htmlFor="image">Post an Image File</label>
+        <label htmlFor="image">Post Image</label>
         <input className="image-input col-one-third" id="image" type="file" name="image" ref={this.fileInputRef} accept=".png, .jpg, .jpeg, .gif" />
         <div className="post-button-holder">
           <button onClick={this.formOpen} className="post-submit-button" type="button">BACK</button>
