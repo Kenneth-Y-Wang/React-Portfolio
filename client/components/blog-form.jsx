0401a338498be508120bb5e9ca28381d49db7cc9
Kenneth Wang <yunfeiw@me.com>
2021-12-13 06:05:26 +0000

    form polish in progress

--- client/components/blog-form.jsx
+++ client/components/blog-form.jsx
@@ -15,10 +15,10 @@ export default class BlogForm extends React.Component {
     return (
       <form className={this.props.blogOpen ? 'blog-form col-four-fifth' : 'hidden'}>
         <label htmlFor="title">Post Title</label>
-        <input required value={this.state.title} id="title" name="title" type="text" placeholder="Please enter your post title..."></input>
+        <input className="blog-form-input" required value={this.state.title} id="title" name="title" type="text" placeholder="Please enter your post title..."></input>
         <textarea required value={this.state.content} id="content" name="content" placeholder="Please enter your post..."></textarea>
         <label htmlFor="image">Post an Image File</label>
-        <input className="image-input" id="image" type="file" name="image" ref={this.fileInputRef} accept=".png, .jpg, .jpeg, .gif" />
+        <input className="image-input col-one-third" id="image" type="file" name="image" ref={this.fileInputRef} accept=".png, .jpg, .jpeg, .gif" />
         <div className="post-button-holder">
           <button onClick={this.formOpen} className="post-submit-button" type="button">BACK</button>
           <button className="post-submit-button" type="submit">POST</button>
