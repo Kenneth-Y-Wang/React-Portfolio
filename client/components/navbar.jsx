129497b15757ccab0e287c198633f83680ec58e0
Kenneth Wang <yunfeiw@me.com>
2022-01-20 03:38:06 +0000

    adding slide menu in progress

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import AppContext from '../lib/app-context';
 
 export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    };
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+  openMenu() {
+    this.setState({ menuOpen: !this.state.menuOpen });
+  }
+
   render() {
     const { route } = this.context;
     return (
@@ -13,8 +25,10 @@ export default class Navbar extends React.Component {
          <a className={route.path === 'blog' ? 'anchor-tag hidden' : 'anchor-tag'} onClick={() => this.props.scroll(this.props.contactMe)} >Contact</a>
          <a className={route.path === 'blog' ? 'anchor-tag' : 'anchor-tag hidden' } href="#" >Home</a>
          <a className="anchor-tag" href="#blog">Blog</a>
+         <button className="anchor-tag menu-button"><i onClick={this.openMenu} className="fas fa-bars"></i></button>
        </div>
       </div>
+      <div className={this.state.menuOpen ? 'menu-modal-holder' : 'menu-modal-holder hidden'}></div>
 
       </>
     );
