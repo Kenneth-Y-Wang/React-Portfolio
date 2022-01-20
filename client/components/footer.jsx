0e1203c9bb3238f1192a2204ace328c758ae2907
Kenneth Wang <yunfeiw@me.com>
2022-01-20 01:49:57 +0000

    footer logo change

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Footer(props) {
   return (
-    <div className="footer">
+    <div style={{ paddingRight: 0 }} className="footer">
       <div className="footer-content" >&copy; Kenneth Wang | Orange County , CA | United States</div>
-      <div className="anchor-holder col-one-third">
-        <a className="footer-content" target="_blank" href="https://www.linkedin.com/in/kenneth-wang8/" rel="noreferrer">LinkedIn</a>
-        <a className="footer-content" target="_blank" href="https://github.com/Kenneth-Y-Wang" rel="noreferrer">GitHub</a>
-        <a className="footer-content" target="_blank" rel="noopener noreferrer" href='mailto:yunfeiw@me.com'>YunfeiW@me.com</a>
+      <div className="anchor-holder col-one-fifth">
+        <a className="footer-content" target="_blank" href="https://www.linkedin.com/in/kenneth-wang8/" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a>
+        <a className="footer-content" target="_blank" href="https://github.com/Kenneth-Y-Wang" rel="noreferrer"><i className="fab fa-github"></i></a>
+        <a className="footer-content" target="_blank" rel="noopener noreferrer" href='mailto:yunfeiw@me.com'><i className="far fa-envelope"></i></a>
       </div>
 
     </div>
