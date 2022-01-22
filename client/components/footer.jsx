70cc2745dc0c8f1c7e1f917b7eb8141129ce2b99
Kenneth Wang <yunfeiw@me.com>
2022-01-22 22:20:41 +0000

    footer correction

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer(props) {
   return (
     <div style={{ paddingRight: 0 }} className="footer">
-      <div className="footer-content" >&copy; Kenneth Wang | Orange County , CA | United States</div>
+      <div className="footer-content" >&copy; Kenneth Wang | OC , CA | United States</div>
       <div className="footer-anchor-holder col-one-fifth">
         <a className="footer-content" target="_blank" href="https://www.linkedin.com/in/kenneth-wang8/" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a>
         <a className="footer-content" target="_blank" href="https://github.com/Kenneth-Y-Wang" rel="noreferrer"><i className="fab fa-github"></i></a>
