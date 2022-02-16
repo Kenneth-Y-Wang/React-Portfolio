b297f001634c0008f8eedd90ed479ad961f075b4
Kenneth Wang <yunfeiw@me.com>
2022-02-16 03:16:25 +0000

    applying hooks in progress

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,50 +1,91 @@
-import React from 'react';
+// import React from 'react';
+// import AppContext from '../lib/app-context';
+
+// export default class Navbar extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       menuOpen: false
+//     };
+//     this.openMenu = this.openMenu.bind(this);
+//   }
+
+//   openMenu() {
+//     if (event.target.matches('.fa-times') || event.target.matches('.slide-tag') || event.target.matches('.fa-bars') || event.target.matches('.menu-modal-holder')) {
+//       this.setState({ menuOpen: !this.state.menuOpen });
+//     }
+//   }
+
+//   render() {
+//     const { route } = this.context;
+//     return (
+//       <>
+//       <div ref={this.props.pageTop} className="nav-bar">
+//        <h3 className="name-title">Kenneth Wang</h3>
+//        <div className="anchor-holder col-one-fifth">
+//          <a className={route.path === 'blog' ? 'anchor-tag hidden' : 'anchor-tag'} onClick={() => this.props.scroll(this.props.myWorks)}>Works</a>
+//          <a className={route.path === 'blog' ? 'anchor-tag hidden' : 'anchor-tag'} onClick={() => this.props.scroll(this.props.contactMe)} >Contact</a>
+//          <a className={route.path === 'blog' ? 'anchor-tag' : 'anchor-tag hidden' } href="#" >Home</a>
+//          <a className="anchor-tag" href="#blog">Blog</a>
+//        </div>
+//        <button className="anchor-tag menu-button"><i onClick={this.openMenu} style={{ fontSize: '1rem' }} className="fas fa-bars"></i></button>
+//        <div onClick={this.openMenu} className={this.state.menuOpen ? 'slide-menu open' : 'slide-menu'}>
+//         <div className="menu-title-holder">
+//           <h2 style={{ fontWeight: 400 }} >MENU</h2>
+//           <button className="slide-menu-exit"><i className="fas fa-times"></i></button>
+//         </div>
+//         <a className={route.path === 'blog' ? ' slide-tag hidden' : ' slide-tag '} onClick={() => this.props.scroll(this.props.myWorks)}>Works</a>
+//         <a className={route.path === 'blog' ? ' slide-tag hidden' : ' slide-tag'} onClick={() => this.props.scroll(this.props.contactMe)} >Contact</a>
+//         <a className={route.path === 'blog' ? ' slide-tag' : ' hidden slide-tag'} href="#" >Home</a>
+//         <a className=" slide-tag" href="#blog">Blog</a>
+//        </div>
+//        <div onClick={this.openMenu} className={this.state.menuOpen ? 'menu-modal-holder' : 'menu-modal-holder hidden'}></div>
+//       </div>
+
+//       </>
+//     );
+//   }
+// }
+
+// Navbar.contextType = AppContext;
+
+import React, { useState, useContext } from 'react';
 import AppContext from '../lib/app-context';
 
-export default class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menuOpen: false
-    };
-    this.openMenu = this.openMenu.bind(this);
-  }
+export default function Navbar(props) {
 
-  openMenu() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const openMenu = event => {
     if (event.target.matches('.fa-times') || event.target.matches('.slide-tag') || event.target.matches('.fa-bars') || event.target.matches('.menu-modal-holder')) {
-      this.setState({ menuOpen: !this.state.menuOpen });
+      setMenuOpen(!menuOpen);
     }
-  }
+  };
 
-  render() {
-    const { route } = this.context;
-    return (
-      <>
-      <div ref={this.props.pageTop} className="nav-bar">
-       <h3 className="name-title">Kenneth Wang</h3>
-       <div className="anchor-holder col-one-fifth">
-         <a className={route.path === 'blog' ? 'anchor-tag hidden' : 'anchor-tag'} onClick={() => this.props.scroll(this.props.myWorks)}>Works</a>
-         <a className={route.path === 'blog' ? 'anchor-tag hidden' : 'anchor-tag'} onClick={() => this.props.scroll(this.props.contactMe)} >Contact</a>
-         <a className={route.path === 'blog' ? 'anchor-tag' : 'anchor-tag hidden' } href="#" >Home</a>
-         <a className="anchor-tag" href="#blog">Blog</a>
-       </div>
-       <button className="anchor-tag menu-button"><i onClick={this.openMenu} style={{ fontSize: '1rem' }} className="fas fa-bars"></i></button>
-       <div onClick={this.openMenu} className={this.state.menuOpen ? 'slide-menu open' : 'slide-menu'}>
-        <div className="menu-title-holder">
-          <h2 style={{ fontWeight: 400 }} >MENU</h2>
-          <button className="slide-menu-exit"><i className="fas fa-times"></i></button>
-        </div>
-        <a className={route.path === 'blog' ? ' slide-tag hidden' : ' slide-tag '} onClick={() => this.props.scroll(this.props.myWorks)}>Works</a>
-        <a className={route.path === 'blog' ? ' slide-tag hidden' : ' slide-tag'} onClick={() => this.props.scroll(this.props.contactMe)} >Contact</a>
-        <a className={route.path === 'blog' ? ' slide-tag' : ' hidden slide-tag'} href="#" >Home</a>
-        <a className=" slide-tag" href="#blog">Blog</a>
-       </div>
-       <div onClick={this.openMenu} className={this.state.menuOpen ? 'menu-modal-holder' : 'menu-modal-holder hidden'}></div>
+  const { route } = useContext(AppContext);
+
+  return (
+  <div ref={props.pageTop} className="nav-bar">
+    <h3 className="name-title">Kenneth Wang</h3>
+    <div className="anchor-holder col-one-fifth">
+      <a className={route.path === 'blog' ? 'anchor-tag hidden' : 'anchor-tag'} onClick={() => props.scroll(props.myWorks)}>Works</a>
+      <a className={route.path === 'blog' ? 'anchor-tag hidden' : 'anchor-tag'} onClick={() => props.scroll(props.contactMe)} >Contact</a>
+      <a className={route.path === 'blog' ? 'anchor-tag' : 'anchor-tag hidden'} href="#" >Home</a>
+      <a className="anchor-tag" href="#blog">Blog</a>
+    </div>
+    <button className="anchor-tag menu-button"><i onClick={openMenu} style={{ fontSize: '1rem' }} className="fas fa-bars"></i></button>
+    <div onClick={openMenu} className={menuOpen ? 'slide-menu open' : 'slide-menu'}>
+      <div className="menu-title-holder">
+        <h2 style={{ fontWeight: 400 }} >MENU</h2>
+        <button className="slide-menu-exit"><i className="fas fa-times"></i></button>
       </div>
+      <a className={route.path === 'blog' ? ' slide-tag hidden' : ' slide-tag '} onClick={() => props.scroll(props.myWorks)}>Works</a>
+      <a className={route.path === 'blog' ? ' slide-tag hidden' : ' slide-tag'} onClick={() => props.scroll(props.contactMe)} >Contact</a>
+      <a className={route.path === 'blog' ? ' slide-tag' : ' hidden slide-tag'} href="#" >Home</a>
+      <a className=" slide-tag" href="#blog">Blog</a>
+    </div>
+    <div onClick={openMenu} className={menuOpen ? 'menu-modal-holder' : 'menu-modal-holder hidden'}></div>
+  </div>
+  );
 
-      </>
-    );
-  }
 }
-
-Navbar.contextType = AppContext;
--- client/components/projects.jsx
+++ client/components/projects.jsx
@@ -1,75 +1,140 @@
-import React from 'react';
-export default class Projects extends React.Component {
-  constructor(props) {
-    super(props);
-    this.thriveVox = React.createRef();
-    this.thriveVoxImg = React.createRef();
-    this.puppy = React.createRef();
-    this.puppyImg = React.createRef();
-    this.showThriveButton = this.showThriveButton.bind(this);
-    this.hideThriveButton = this.hideThriveButton.bind(this);
-    this.showPuppyButton = this.showPuppyButton.bind(this);
-    this.hidePuppyButton = this.hidePuppyButton.bind(this);
+// import React from 'react';
+// export default class Projects extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.thriveVox = React.createRef();
+//     this.thriveVoxImg = React.createRef();
+//     this.puppy = React.createRef();
+//     this.puppyImg = React.createRef();
+//     this.showThriveButton = this.showThriveButton.bind(this);
+//     this.hideThriveButton = this.hideThriveButton.bind(this);
+//     this.showPuppyButton = this.showPuppyButton.bind(this);
+//     this.hidePuppyButton = this.hidePuppyButton.bind(this);
 
-  }
+//   }
 
-  showThriveButton() {
-    this.thriveVox.current.className = 'project-button-holder';
-    this.thriveVoxImg.current.className = ' overlay';
+//   showThriveButton() {
+//     this.thriveVox.current.className = 'project-button-holder';
+//     this.thriveVoxImg.current.className = ' overlay';
 
-  }
+//   }
 
-  hideThriveButton() {
-    this.thriveVox.current.className = 'project-button-holder hidden';
-    this.thriveVoxImg.current.className = 'overlay hidden';
+//   hideThriveButton() {
+//     this.thriveVox.current.className = 'project-button-holder hidden';
+//     this.thriveVoxImg.current.className = 'overlay hidden';
 
-  }
+//   }
 
-  showPuppyButton() {
-    this.puppy.current.className = 'project-button-holder';
-    this.puppyImg.current.className = ' img-hover';
+//   showPuppyButton() {
+//     this.puppy.current.className = 'project-button-holder';
+//     this.puppyImg.current.className = ' img-hover';
 
-  }
+//   }
 
-  hidePuppyButton() {
-    this.puppy.current.className = 'project-button-holder hidden';
-    this.puppyImg.current.className = '';
+//   hidePuppyButton() {
+//     this.puppy.current.className = 'project-button-holder hidden';
+//     this.puppyImg.current.className = '';
 
-  }
+//   }
 
-  render() {
-    return (
-      <div className="container">
-        <div ref={this.props.myWorks} className="title-row col-full">
-          <div className="title-content">
-            <h1>My Works</h1>
-            <div className="title-sub">Click Image for Details</div>
+//   render() {
+//     return (
+//       <div className="container">
+//         <div ref={this.props.myWorks} className="title-row col-full">
+//           <div className="title-content">
+//             <h1>My Works</h1>
+//             <div className="title-sub">Click Image for Details</div>
+//           </div>
+//         </div>
+//         <div className="about-row">
+//           <div onMouseEnter={this.showThriveButton} onMouseLeave={this.hideThriveButton} className="col-half project-image-holder">
+//             <img src="./images/150-1508074_black-and-white-music-headphones-life-hd-grayscale.jpg"></img>
+//             <div ref={this.thriveVoxImg} className="overlay hidden"></div>
+//             <div ref={this.thriveVox} className="project-button-holder hidden">
+//               <h1 className="col-four-fifth">ThriveVox</h1>
+//               <div style={{ color: '#f8f9fa' }} className=" project-content-holder">
+//                <p>A full-stack web application for musicians and music lovers who want to find single/band members, share and discuss their musical interests and favorites</p>
+//               </div>
+//               <a target="_blank" href="https://thrive-vox.herokuapp.com/" rel="noreferrer" className="skill-button">Enter App</a>
+//             </div>
+//           </div>
+//           <div onMouseEnter={this.showPuppyButton} onMouseLeave={this.hidePuppyButton} className="col-half project-image-holder">
+//             <img ref={this.puppyImg} src="./images/GettyImages-1133605325-scaled-e1617227898456.jpg"></img>
+//             <div ref={this.puppy} className="project-button-holder hidden">
+//               <h1 style={{ color: '#495057' }} className="col-four-fifth">Puppy Paradise</h1>
+//               <div className=" project-content-holder">
+//                 <p>A front-end web application for puppy lovers who seek entertainment from cute pictures, fun games, and organize their own pets’ daily activities</p>
+//               </div>
+//               <a target="_blank" href="https://kenneth-y-wang.github.io/puppy-paradise/" rel="noreferrer" className="about-button">Enter App</a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+import React, { useRef } from 'react';
+
+export default function Projects(props) {
+
+  const thriveVox = useRef(null);
+  const thriveVoxImg = useRef(null);
+  const puppy = useRef(null);
+  const puppyImg = useRef(null);
+
+  const showThriveButton = () => {
+    thriveVox.current.className = 'project-button-holder';
+    thriveVoxImg.current.className = ' overlay';
+  };
+
+  const hideThriveButton = () => {
+    thriveVox.current.className = 'project-button-holder hidden';
+    thriveVoxImg.current.className = 'overlay hidden';
+  };
+
+  const showPuppyButton = () => {
+    puppy.current.className = 'project-button-holder';
+    puppyImg.current.className = ' img-hover';
+  };
+
+  const hidePuppyButton = () => {
+    puppy.current.className = 'project-button-holder hidden';
+    puppyImg.current.className = '';
+
+  };
+
+  return (
+  <div className="container">
+    <div ref={props.myWorks} className="title-row col-full">
+      <div className="title-content">
+        <h1>My Works</h1>
+        <div className="title-sub">Click Image for Details</div>
+      </div>
+    </div>
+    <div className="about-row">
+      <div onMouseEnter={showThriveButton} onMouseLeave={hideThriveButton} className="col-half project-image-holder">
+        <img src="./images/150-1508074_black-and-white-music-headphones-life-hd-grayscale.jpg"></img>
+        <div ref={thriveVoxImg} className="overlay hidden"></div>
+        <div ref={thriveVox} className="project-button-holder hidden">
+          <h1 className="col-four-fifth">ThriveVox</h1>
+          <div style={{ color: '#f8f9fa' }} className=" project-content-holder">
+            <p>A full-stack web application for musicians and music lovers who want to find single/band members, share and discuss their musical interests and favorites</p>
           </div>
-        </div>
-        <div className="about-row">
-          <div onMouseEnter={this.showThriveButton} onMouseLeave={this.hideThriveButton} className="col-half project-image-holder">
-            <img src="./images/150-1508074_black-and-white-music-headphones-life-hd-grayscale.jpg"></img>
-            <div ref={this.thriveVoxImg} className="overlay hidden"></div>
-            <div ref={this.thriveVox} className="project-button-holder hidden">
-              <h1 className="col-four-fifth">ThriveVox</h1>
-              <div style={{ color: '#f8f9fa' }} className=" project-content-holder">
-               <p>A full-stack web application for musicians and music lovers who want to find single/band members, share and discuss their musical interests and favorites</p>
-              </div>
-              <a target="_blank" href="https://thrive-vox.herokuapp.com/" rel="noreferrer" className="skill-button">Enter App</a>
-            </div>
-          </div>
-          <div onMouseEnter={this.showPuppyButton} onMouseLeave={this.hidePuppyButton} className="col-half project-image-holder">
-            <img ref={this.puppyImg} src="./images/GettyImages-1133605325-scaled-e1617227898456.jpg"></img>
-            <div ref={this.puppy} className="project-button-holder hidden">
-              <h1 style={{ color: '#495057' }} className="col-four-fifth">Puppy Paradise</h1>
-              <div className=" project-content-holder">
-                <p>A front-end web application for puppy lovers who seek entertainment from cute pictures, fun games, and organize their own pets’ daily activities</p>
-              </div>
-              <a target="_blank" href="https://kenneth-y-wang.github.io/puppy-paradise/" rel="noreferrer" className="about-button">Enter App</a>
-            </div>
-          </div>
+          <a target="_blank" href="https://thrive-vox.herokuapp.com/" rel="noreferrer" className="skill-button">Enter App</a>
         </div>
       </div>
-    );
-  }
+      <div onMouseEnter={showPuppyButton} onMouseLeave={hidePuppyButton} className="col-half project-image-holder">
+        <img ref={puppyImg} src="./images/GettyImages-1133605325-scaled-e1617227898456.jpg"></img>
+        <div ref={puppy} className="project-button-holder hidden">
+          <h1 style={{ color: '#495057' }} className="col-four-fifth">Puppy Paradise</h1>
+          <div className=" project-content-holder">
+            <p>A front-end web application for puppy lovers who seek entertainment from cute pictures, fun games, and organize their own pets’ daily activities</p>
+          </div>
+          <a target="_blank" href="https://kenneth-y-wang.github.io/puppy-paradise/" rel="noreferrer" className="about-button">Enter App</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
 }
