3895cc9f14e69fcdb47da462fd1760993d68fd7f
Kenneth Wang <yunfeiw@me.com>
2022-02-17 23:56:59 +0000

    applying hooks

--- client/components/comments.jsx
+++ client/components/comments.jsx
@@ -1,25 +1,186 @@
-import React from 'react';
+// import React from 'react';
+// import { format } from 'date-fns';
+// import AppContext from '../lib/app-context';
+
+// export default class Comments extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       allComments: [],
+//       input: '',
+//       inputError: false,
+//       displayComment: this.props.displayComment
+
+//     };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleDelete = this.handleDelete.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   componentDidMount() {
+
+//     fetch(`/api/comments/allComments/${this.props.postId}`, {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+
+//     })
+//       .then(response => response.json())
+//       .then(data => {
+//         this.setState({ allComments: data });
+//       })
+//       .catch(error => {
+//         console.error('Error:', error);
+//       });
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.allComments !== this.state.allComments) {
+
+//       fetch(`/api/comments/allComments/${this.props.postId}`, {
+//         method: 'GET',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         }
+
+//       })
+//         .then(response => response.json())
+//         .then(data => {
+//           this.setState({ allComments: data });
+//         })
+//         .catch(error => {
+//           console.error('Error:', error);
+//         });
+//     }
+
+//     if (prevProps.displayComment !== this.props.displayComment) {
+//       this.setState({ inputError: false });
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     this.setState({ inputError: false });
+//   }
+
+//   handleDelete(commentId) {
+//     const token = window.localStorage.getItem('react-context-jwt');
+//     fetch(`/api/comments/allCommentsToDelete/${commentId}`, {
+//       method: 'DELETE',
+//       headers: {
+//         'react-context-jwt': token,
+//         'Content-Type': 'application/json'
+//       },
+//       body: null
+//     });
+
+//     for (let i = 0; i < this.state.allComments.length; i++) {
+//       if (commentId === this.state.allComments[i].commentId) {
+//         const newState = this.state.allComments.slice(0, i).concat(this.state.allComments.slice(i + 1));
+//         this.setState({ allComments: newState });
+//         break;
+//       }
+
+//     }
+//   }
+
+//   handleChange(event) {
+//     if (this.context.user) {
+//       this.setState({ inputError: false });
+//     }
+//     this.setState({ input: event.target.value });
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     if (!this.context.user) {
+//       this.setState({ inputError: true, input: '' });
+//     } else {
+
+//       const token = window.localStorage.getItem('react-context-jwt');
+//       const newTime = format(new Date(), 'yyyy-MM-dd HH:mm');
+
+//       const newComment = {
+//         content: this.state.input,
+//         postId: this.props.postId,
+//         time: newTime
+//       };
+//       fetch('/api/comments/create', {
+//         method: 'POST',
+//         headers: {
+//           'react-context-jwt': token,
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(newComment)
+//       })
+//         .then(response => response.json())
+//         .then(data => {
+//           const newSavedComment = {
+//             commentId: data.commentId,
+//             content: data.content,
+//             createdAt: data.createdAt,
+//             username: this.context.user.username
+//           };
+//           this.setState({
+//             allComments: this.state.allComments.concat(newSavedComment),
+//             inputError: false
+//           });
+//         })
+//         .catch(error => {
+//           console.error('error', error);
+//         });
+//       this.setState({ input: '' });
+//     }
+//   }
+
+//   render() {
+//     const allComments = this.state.allComments;
+//     const commentList = allComments.map(comment => {
+//       const { commentId, content, createdAt, username } = comment;
+//       const checkedUser = this.context.user
+//         ? this.context.user.username
+//         : '';
+//       const date = createdAt.slice(0, 10) + ' ' + createdAt.slice(11, 16);
+
+//       return (
+//         <li className="comment-detail-list" key={commentId}>{username}<span className="comment-date"> {date}</span> : <span className="comment-detail">{content}</span>
+//           <button onClick={() => this.handleDelete(commentId)} type="button" className={username === checkedUser ? 'comment-delete-button' : 'comment-delete-button hidden'}>DELETE</button>
+//         </li>
+//       );
+//     });
+//     return (
+//       <>
+//         <form onSubmit={this.handleSubmit}>
+//           <input className="comment-input" onChange={this.handleChange} type="text" id="comment" name="comment" value={this.state.input}
+//             placeholder="please enter your comment..."></input>
+//           <div className={this.state.inputError ? 'comment-error-message' : 'comment-error-message hidden'}>Please sign in first to leave comment...</div>
+//         </form>
+//         <h4 className="recent-comment" style={{ fontWeight: 500 }}>Recent Comments:</h4>
+//         <ul className="comment-list">
+//           {this.state.allComments.length !== 0
+//             ? commentList
+//             : <li>No comment for this post...</li>}
+//         </ul>
+//       </>
+//     );
+//   }
+// }
+
+// Comments.contextType = AppContext;
+
+import React, { useState, useEffect, useContext } from 'react';
 import { format } from 'date-fns';
 import AppContext from '../lib/app-context';
 
-export default class Comments extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      allComments: [],
-      input: '',
-      inputError: false,
-      displayComment: this.props.displayComment
+export default function Comments(props) {
 
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const [allComments, setAllComments] = useState([]);
+  const [input, setInput] = useState('');
+  const [inputError, setInputError] = useState(false);
+  const [displayComment] = useState(props.displayComment);
 
-  componentDidMount() {
-
-    fetch(`/api/comments/allComments/${this.props.postId}`, {
+  useEffect(() => {
+    fetch(`/api/comments/allComments/${props.postId}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
@@ -28,42 +189,23 @@ export default class Comments extends React.Component {
     })
       .then(response => response.json())
       .then(data => {
-        this.setState({ allComments: data });
+        setAllComments(data);
       })
       .catch(error => {
         console.error('Error:', error);
       });
-  }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.allComments !== this.state.allComments) {
+    return () => {
+      setInputError(false);
+    };
+  }, []);
 
-      fetch(`/api/comments/allComments/${this.props.postId}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
+  useEffect(() => {
+    setInputError(false);
+    console.log('hello');
+  }, [displayComment]);
 
-      })
-        .then(response => response.json())
-        .then(data => {
-          this.setState({ allComments: data });
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    }
-
-    if (prevProps.displayComment !== this.props.displayComment) {
-      this.setState({ inputError: false });
-    }
-  }
-
-  componentWillUnmount() {
-    this.setState({ inputError: false });
-  }
-
-  handleDelete(commentId) {
+  const handleDelete = commentId => {
     const token = window.localStorage.getItem('react-context-jwt');
     fetch(`/api/comments/allCommentsToDelete/${commentId}`, {
       method: 'DELETE',
@@ -74,35 +216,38 @@ export default class Comments extends React.Component {
       body: null
     });
 
-    for (let i = 0; i < this.state.allComments.length; i++) {
-      if (commentId === this.state.allComments[i].commentId) {
-        const newState = this.state.allComments.slice(0, i).concat(this.state.allComments.slice(i + 1));
-        this.setState({ allComments: newState });
+    for (let i = 0; i < allComments.length; i++) {
+      if (commentId === allComments[i].commentId) {
+        const newState = allComments.slice(0, i).concat(allComments.slice(i + 1));
+        setAllComments(newState);
         break;
       }
 
     }
-  }
+  };
+  const { user } = useContext(AppContext);
 
-  handleChange(event) {
-    if (this.context.user) {
-      this.setState({ inputError: false });
+  const handleChange = event => {
+    if (user) {
+      setInputError(false);
     }
-    this.setState({ input: event.target.value });
-  }
 
-  handleSubmit(event) {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = event => {
     event.preventDefault();
-    if (!this.context.user) {
-      this.setState({ inputError: true, input: '' });
+    if (!user) {
+      setInputError(true);
+      setInput('');
     } else {
 
       const token = window.localStorage.getItem('react-context-jwt');
       const newTime = format(new Date(), 'yyyy-MM-dd HH:mm');
 
       const newComment = {
-        content: this.state.input,
-        postId: this.props.postId,
+        content: input,
+        postId: props.postId,
         time: newTime
       };
       fetch('/api/comments/create', {
@@ -119,51 +264,47 @@ export default class Comments extends React.Component {
             commentId: data.commentId,
             content: data.content,
             createdAt: data.createdAt,
-            username: this.context.user.username
+            username: user.username
           };
-          this.setState({
-            allComments: this.state.allComments.concat(newSavedComment),
-            inputError: false
-          });
+          setAllComments(allComments.concat(newSavedComment));
+          setInputError(false);
+
         })
         .catch(error => {
           console.error('error', error);
         });
-      this.setState({ input: '' });
+      setInput('');
     }
-  }
+  };
 
-  render() {
-    const allComments = this.state.allComments;
-    const commentList = allComments.map(comment => {
-      const { commentId, content, createdAt, username } = comment;
-      const checkedUser = this.context.user
-        ? this.context.user.username
-        : '';
-      const date = createdAt.slice(0, 10) + ' ' + createdAt.slice(11, 16);
+  const commentList = allComments.map(comment => {
+    const { commentId, content, createdAt, username } = comment;
+    const checkedUser = user
+      ? user.username
+      : '';
+    const date = createdAt.slice(0, 10) + ' ' + createdAt.slice(11, 16);
 
-      return (
-        <li className="comment-detail-list" key={commentId}>{username}<span className="comment-date"> {date}</span> : <span className="comment-detail">{content}</span>
-          <button onClick={() => this.handleDelete(commentId)} type="button" className={username === checkedUser ? 'comment-delete-button' : 'comment-delete-button hidden'}>DELETE</button>
-        </li>
-      );
-    });
     return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <input className="comment-input" onChange={this.handleChange} type="text" id="comment" name="comment" value={this.state.input}
-            placeholder="please enter your comment..."></input>
-          <div className={this.state.inputError ? 'comment-error-message' : 'comment-error-message hidden'}>Please sign in first to leave comment...</div>
-        </form>
-        <h4 className="recent-comment" style={{ fontWeight: 500 }}>Recent Comments:</h4>
-        <ul className="comment-list">
-          {this.state.allComments.length !== 0
-            ? commentList
-            : <li>No comment for this post...</li>}
-        </ul>
-      </>
+     <li className="comment-detail-list" key={commentId}>{username}<span className="comment-date"> {date}</span> : <span className="comment-detail">{content}</span>
+       <button onClick={() => handleDelete(commentId)} type="button" className={username === checkedUser ? 'comment-delete-button' : 'comment-delete-button hidden'}>DELETE</button>
+     </li>
     );
-  }
-}
+  });
 
-Comments.contextType = AppContext;
+  return (
+   <>
+     <form onSubmit={handleSubmit}>
+       <input className="comment-input" onChange={handleChange} type="text" id="comment" name="comment" value={input}
+         placeholder="please enter your comment..."></input>
+       <div className={inputError ? 'comment-error-message' : 'comment-error-message hidden'}>Please sign in first to leave comment...</div>
+     </form>
+     <h4 className="recent-comment" style={{ fontWeight: 500 }}>Recent Comments:</h4>
+     <ul className="comment-list">
+       {allComments.length !== 0
+         ? commentList
+         : <li>No comment for this post...</li>}
+     </ul>
+   </>
+  );
+
+}
