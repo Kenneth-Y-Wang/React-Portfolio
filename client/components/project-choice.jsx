7bc1e80a4c2639990873106857098822b0d31267
Kenneth Wang <yunfeiw@me.com>
2022-08-25 04:03:57 +0000

    switch view in progress

--- client/components/project-choice.jsx
+++ client/components/project-choice.jsx
@@ -1,15 +1,29 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const ProjectChoice = ({
   brief,
   detail
 }) => {
 
+  const [projectLayout, setProjectLayout] = useState('all');
+  const detailLayout = useRef(null);
+
+  const setLayout = () => {
+    if (projectLayout === 'all') {
+      setProjectLayout('detail');
+      detailLayout.current.className = 'choice-button choice-button-right';
+    } else {
+      setProjectLayout('all');
+      detailLayout.current.className = 'choice-button';
+    }
+  };
+
   return (
     <section className="project-choice-row">
-      <div className="choice-icon"><i className="fas fa-boxes"></i></div>
-      <div className="choice-icon"><i className="fas fa-box"></i></div>
+      <div className="choice-icon" onClick={setLayout}><i className="fas fa-boxes"></i></div>
+      <div className="choice-icon" onClick={setLayout}><i className="fas fa-box"></i></div>
+      <div ref={detailLayout} className="choice-button" />
     </section>
   );
 };
