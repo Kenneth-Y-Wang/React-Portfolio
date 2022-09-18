35ba46e654114096c8719cae3b19da27c8525c9d
Kenneth Wang <yunfeiw@me.com>
2022-09-18 03:41:23 +0000

    heading in progress

--- client/components/project-detail-view.jsx
+++ client/components/project-detail-view.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Row from './portfolio-row';
 import Column from './portfolio-column';
 import Box from './portfolio-box';
+import Heading from './heading';
 
 const ProjectDetailView = () => {
 
@@ -15,9 +16,11 @@ const ProjectDetailView = () => {
   return (
   <div className="container">
     <Row rowPadding={true} isWrapped={false}>
-      <Column span={'6'}>
+      <Column span={'10'}>
           {/* <div style={{ backgroundColor: '#495057', width: '100%', height: '100px' }} /> */}
-          <Box backgroundColor={'mobile:#495057,tablet:black'} width={'mobile:50%,tablet:100%'} height={'100px'} />
+          <Box backgroundColor={'mobile:#495057,tablet:white'} width={'mobile:50%,tablet:100%'} height={'100px'} >
+            <Heading type={'h2'} weight={'200'} size={'5rem'} color={'red'} textAlign={'center'}>Hello world</Heading>
+            </Box>
       </Column>
     </Row>
   </div>
