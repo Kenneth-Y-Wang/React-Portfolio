7b2c95a7d8ed1d06a4bace9478c642c97a6bed91
Kenneth Wang <yunfeiw@me.com>
2022-09-20 03:52:24 +0000

    background image component in progress

---
+++ client/components/background-image.jsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import useMediaQuery from '../hooks/use-media-query';
+import valueConvert from '../lib/mobile-tablet-value-convert';
+
+export default function BackgroundImage({
+  url,
+  imagePositionX,
+  imagePositionY,
+  imageSize,
+  width,
+  height,
+  className
+}) {
+  const isTablet = useMediaQuery('(min-width: 768px)');
+  const [urlMobile, urlTablet] = valueConvert(url);
+  const [positionXMobile, positionXTablet] = valueConvert(imagePositionX);
+  const [positionYMobile, positionYTablet] = valueConvert(imagePositionY);
+  const [sizeMobile, sizeTablet] = valueConvert(imageSize);
+  const [widthMobile, widthTablet] = valueConvert(width);
+  const [heightMobile, heightTablet] = valueConvert(height);
+
+  const backgroundStyle = {
+    box: isTablet => ({
+      backgroundImage: isTablet ? urlTablet : urlMobile,
+      backgroundPositionX: isTablet ? positionXTablet : positionXMobile,
+      backgroundPositionY: isTablet ? positionYTablet : positionYMobile,
+      backgroundSize: isTablet ? sizeTablet : sizeMobile,
+      height: isTablet ? heightTablet : heightMobile,
+      width: isTablet ? widthTablet : widthMobile
+    })
+  };
+
+  return (
+   <div style={backgroundStyle.box(isTablet)} className = {className} />
+  );
+
+}
