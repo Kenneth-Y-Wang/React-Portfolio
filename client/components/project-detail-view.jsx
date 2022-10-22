c6a7c578931f4c4476485f50859a2bfc9d0fb2d8
Kenneth Wang <yunfeiw@me.com>
2022-10-22 19:08:47 +0000

    column fixed

--- client/components/project-detail-view.jsx
+++ client/components/project-detail-view.jsx
@@ -17,11 +17,19 @@ const ProjectDetailView = () => {
   return (
   <div className="container">
     <Row rowPadding={true} isWrapped={true}>
-      <Column span={'mobile:12,tablet:10'} offset={'mobile:0,tablet:2'}>
+      <Column span={'mobile:12,tablet:4'} offset={'mobile:0,tablet:2'}>
         {/* <div style={{ backgroundColor: '#495057', width: '100%', height: '100px' }} /> */}
         <Box backgroundColor={'mobile:#495057,tablet:white'} width={'100%'} height={'100px'} >
           {/* <Heading type={'h5'} weight={'200'} size={'2rem'} color={'blue'} textAlign={'center'}>Hello Jimin</Heading> */}
         </Box>
+      </Column>
+
+      <Column span={'mobile:12,tablet:6'} >
+        {/* <div style={{ backgroundColor: '#495057', width: '100%', height: '100px' }} /> */}
+        <BackgroundImage url={'mobile:url("./images/pexels-photo-234527.jpeg"),tablet:url("./images/pexels-hasan-albari-1229861.jpg")'} width={'100%'}
+         height={'100px'} imageSize={'cover'}>
+          {/* <Heading type={'h5'} weight={'200'} size={'2rem'} color={'blue'} textAlign={'center'}>Hello Jimin</Heading> */}
+        </BackgroundImage>
       </Column>
     </Row>
   </div>
