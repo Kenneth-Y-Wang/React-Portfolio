62c95aec6115b3fa8af3acabe4fdcb0c285a394a
Kenneth Wang <yunfeiw@me.com>
2022-11-13 01:52:50 +0000

    start the play-ground

---
+++ client/app-play-ground.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './pages/home';
+
+export default function AppPlayGround() {
+
+  return (
+       <h1>Hello, World!</h1>
+  );
+
+}
--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import AppPlayGround from './app-play-ground';
 
 ReactDOM.render(
-  <App />,
+  <AppPlayGround />,
   document.querySelector('#root')
 );
